647040f6c8c783d46f3970c0267115460ce20c71
ThibClnt <thibaut.colnot@efrei.net>
2024-12-11 17:51:57 +0100

    #4 Add basic layout for anonymous users

---
+++ front/trip-planner/components/anonymous/menu.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const links = [
+  [
+    {
+      label: 'Home',
+      to: '/'
+    },
+    {
+      label: 'About',
+      to: '/about'
+    }
+  ],
+  [
+    {
+      label: 'Login',
+      to: '/login'
+    },
+    {
+      label: 'Register',
+      to: '/register'
+    }
+    ],
+];
+</script>
+
+<template>
+  <UHorizontalNavigation class="flex-grow" :links />
+</template>
---
+++ front/trip-planner/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-gray-100 dark:bg-gray-900">
+    <UContainer class="flex flex-col space-y-3 min-h-screen sm:px-24">
+      <AnonymousHeader class="mt-2 mb-3" />
+      <div class="flex-1">
+        <NuxtPage />
+      </div>
+      <Footer class="mb-3" />
+    </UContainer>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
