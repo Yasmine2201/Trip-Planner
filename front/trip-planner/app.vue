a2dc8e04d3275fcccdcd080ffd7c6c66e2ae5d7f
ThibClnt <thibaut.colnot@efrei.net>
2024-12-13 13:06:47 +0100

    Working CORS configuration - dev

--- front/trip-planner/app.vue
+++ front/trip-planner/app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    {{ data }}
   </div>
 </template>
+
+<script lang="ts" setup>
+console.log("HEY")
+const { data } = await useFetch("http://localhost:8000/auth/");
+</script>
