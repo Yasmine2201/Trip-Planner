afd31d3ec3d35781d61c0c3c67a088239d819a9a
ThibClnt <thibaut.colnot@efrei.net>
2024-12-13 13:06:47 +0100

    Working CORS configuration - dev

--- back/tripplanner/settings.py
+++ back/tripplanner/settings.py
@@ -43,22 +43,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'authentication'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-]
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://notre-domaine.com",
 ]
 
 ROOT_URLCONF = 'tripplanner.urls'
@@ -137,3 +132,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS settings
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
