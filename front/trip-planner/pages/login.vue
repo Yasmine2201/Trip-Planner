1f9e06126f7b1743ba4ad104929b67e979f0bff8
ThibClnt <thibaut.colnot@efrei.net>
2024-12-13 14:03:39 +0100

    Working CORS configuration - with layout

---
+++ front/trip-planner/pages/login.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  console.log("HEY")
+  const { data } = await useFetch("http://localhost:8000/auth/");
+</script>
+
+<template>
+  <div class="text-primary">
+    {{ data }}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
