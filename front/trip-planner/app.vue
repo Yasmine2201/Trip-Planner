f2b5af8f61693c017f55e5f0d051358c4588d529
ThibClnt <thibaut.colnot@efrei.net>
2024-12-13 19:04:48 +0100

    Front - Add auth layout, change favicon, add page title

--- front/trip-planner/app.vue
+++ front/trip-planner/app.vue
@@ -3,3 +3,16 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+  const { $router } = useNuxtApp();
+
+  const getTitle = computed(() => {
+    const pageName = $router.currentRoute.value.meta.title;
+    return `${pageName} • TripPlanner`;
+  });
+
+  useHead({
+    title: getTitle
+  });
+</script>
