bd16b5d812eac2bb92167a2444645b4b3cae1406
ThibClnt <thibaut.colnot@efrei.net>
2024-12-19 03:45:25 +0100

    Login endpoint

---
+++ back/authentication/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+
+class AuthErrorSerializer(serializers.Serializer):
+    msg = serializers.CharField()
+    error_code = serializers.CharField()
+
+
+class LoginInputSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+
+
+class UserSerializer(serializers.Serializer):
+    id = serializers.CharField(source='user.id')
+    email = serializers.EmailField(source='user.email')
+    role = serializers.CharField(source='user.role')
+    expires_in = serializers.IntegerField()
+    expires_at = serializers.IntegerField()
--- back/authentication/views.py
+++ back/authentication/views.py
@@ -1,5 +1,40 @@
-from django.http import JsonResponse
+from pydantic import ValidationError
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-def hello(request):
-    data = {"message": "Hello, Auth"}
-    return JsonResponse(data)
+from authentication.serializers import LoginInputSerializer, AuthErrorSerializer, UserSerializer
+from authentication.utils import set_supabase_cookies
+from utils.auth_client import AuthClient, AuthSession, AuthException, InvalidCredentialsException
+
+
+class LoginView(APIView):
+
+    @staticmethod
+    def post(request: Request):
+        try:
+            request_serializer = LoginInputSerializer(data=request.data)
+            if not request_serializer.is_valid():
+                error_serializer = AuthErrorSerializer({
+                    "msg": request_serializer.errors,
+                    "error_code": "invalid_request_data"
+                })
+                return Response(error_serializer.data, status=400)
+
+            email: str = request_serializer.validated_data['email']
+            password: str = request_serializer.validated_data['password']
+
+            auth_response: AuthSession = AuthClient().login(email, password)
+            response_serializer = UserSerializer(auth_response)
+
+            response = Response(response_serializer.data, status=200)
+            set_supabase_cookies(response, auth_response)
+            return response
+
+        except InvalidCredentialsException as e:
+            error_serializer = AuthErrorSerializer(e)
+            return Response(error_serializer.data, status=401)
+
+        except AuthException as e:
+            error_serializer = AuthErrorSerializer(e)
+            return Response(error_serializer.data, status=400)
