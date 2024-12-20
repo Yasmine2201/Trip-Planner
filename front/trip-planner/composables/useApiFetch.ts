65c1abe0cc40cd5aca865e84341e82fb803b6ad9
ThibClnt <thibaut.colnot@efrei.net>
2024-12-20 21:31:13 +0100

    Mise à jour useApiFetch.ts : ne gère plus le refresh du token (#57)

--- front/trip-planner/composables/useApiFetch.ts
+++ front/trip-planner/composables/useApiFetch.ts
@@ -1,7 +1,5 @@
 export function useApiFetch<T>(url: string, options: RequestInit = {}) {
   const config = useRuntimeConfig();
-  const auth = useAuthStore();
-
   return useFetch<T>(url, {
       baseURL: config.public.apiBaseUrl,
       credentials: 'include',
@@ -10,22 +8,5 @@ export function useApiFetch<T>(url: string, options: RequestInit = {}) {
         ...(options.headers || {}),
       },
       ...options,
-    onResponseError: async (error) => {
-      if (error.response && error.response.status === 401) {
-        return auth.refreshToken().then(() => {
-          return useFetch<T>(url, {
-            baseURL: config.public.apiBaseUrl,
-            credentials: 'include',
-            headers: {
-              'Content-Type': 'application/json',
-              ...(options.headers || {}),
-            },
-            ...options
-          });
-        }).catch(() => {
-          return Promise.reject('Token refresh failed');
-        });
-      }
-    }
   });
 }
