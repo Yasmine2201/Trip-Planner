a7ac4bf3fc41fa6ba4d78277c6ed78265841c58e
Yasmine <yasmineazib66@gmail.com>
2024-12-21 02:07:34 +0100

    get trips done

--- back/trips/serializers.py
+++ back/trips/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
-from trips.models import Trip
+from trips.models import Trip, TripParticipation
 
 
 class TripSerializer(serializers.ModelSerializer):
     class Meta:
         model = Trip
+        fields = '__all__'
+
+class TripParticipationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TripParticipation
         fields = '__all__'
