24315753f0070107d7d47ba962d4aa3df89e1e0f
Yasmine Azib <118924813+Yasmine2201@users.noreply.github.com>
2024-12-24 20:42:56 +0100

    Added get and delete notification (#66)

---
+++ back/notifications/services.py
@@ -0,0 +1,36 @@
+from core.models import User
+from notifications.models import Notification
+
+
+class NotificationService:
+
+    @staticmethod
+    def get_all_user_notifications(user: User):
+        """
+        Fetch all notifications associated with a user.
+        """
+
+        return Notification.objects.filter(user=user.user_id)
+
+    @staticmethod
+    def get_user_notification(user: User, notification_id: int):
+        """
+        Fetch a specific notification associated with a user and set it as read.
+        """
+
+        notification = Notification.objects.get(user=user.user_id, notification_id=notification_id)
+        if not notification.read:
+            notification.read = True
+            notification.save()
+        return notification
+
+    @staticmethod
+    def delete_user_notification(user: User, notification_id: int):
+        """
+        Delete a specific notification associated with a user.
+        """
+
+        notification = Notification.objects.get(user=user.user_id, notification_id=notification_id)
+        notification.delete()
+        return notification
+
--- back/tripplanner/settings.py
+++ back/tripplanner/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    'trips'
+    'trips',
+    'notifications'
 ]
 
 MIDDLEWARE = [
