9874537f3deab5d5d55ff5394a50b5d040df72f9
ThibClnt <thibaut.colnot@efrei.net>
2024-12-30 00:54:15 +0100

    Ajout navigation générale

---
+++ front/trip-planner/components/NavigationMenu.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+const { t } = useI18n();
+const authStore = useAuthStore();
+
+const links = computed(() => {
+  return [[
+    {
+      label: t('navigation.profile'),
+      icon: "i-uil-user",
+      to: "/home/profile/me"
+    }
+  ], [
+    {
+      label: t('navigation.my-trips'),
+      icon: "material-symbols:trip-outline",
+      to: "/home"
+    },
+    {
+      label: t('navigation.last-trip'),
+      icon: "heroicons:clock",
+      to: "/home/last-trip"
+    },
+    {
+      label: t('navigation.new-trip'),
+      icon: "heroicons:plus",
+      to: '/home/new-trip'
+    }
+  ],[{
+    label: t('navigation.logout'),
+    icon: "i-uil-signout",
+    click: logout
+  }
+  ]];
+});
+
+const logout = async () => {
+  await authStore.logout();
+}
+
+</script>
+
+<template>
+  <UVerticalNavigation
+    :links="links"
+    class="nav"
+    :ui="{
+      active: 'text-primary-500 dark:text-primary-400 font-medium',
+      inactive: 'text-gray-500 dark:text-gray-400 font-normal',
+    }"
+  />
+</template>
+
+<style scoped>
+.nav {
+  padding: 1rem;
+  height: 100%;
+  box-shadow: 2px 1px 2px rgba(0, 0, 0, 0.1);
+}
+</style>
---
+++ front/trip-planner/layouts/navigation.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-gray-100 dark:bg-gray-900" flex>
+    <UContainer class="flex flex-col space-y-3 min-h-screen sm:px-24">
+      <Header class="mt-2 mb-3"/>
+      <div class="flex-1 h-full flex flex-row">
+        <div class="xl:w-1/5 lg:w-1/4 sm:w-1/3 h-full mr-4">
+          <NavigationMenu class="h-full" />
+        </div>
+        <div class="xl:w-4/5 lg:w-3/4 sm:w-2/3">
+          <NuxtPage />
+        </div>
+      </div>
+      <Footer class="mb-3" />
+    </UContainer>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
