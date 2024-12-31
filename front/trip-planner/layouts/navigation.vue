3094f719eb4bd7075f1ecef7eddea29b7da2a9b2
ThibClnt <thibaut.colnot@efrei.net>
2024-12-31 12:38:02 +0100

    Trip list - Delete to do

--- front/trip-planner/layouts/navigation.vue
+++ front/trip-planner/layouts/navigation.vue
@@ -2,7 +2,7 @@
   <div class="bg-gray-100 dark:bg-gray-900" flex>
     <UContainer class="flex flex-col space-y-3 min-h-screen sm:px-24">
       <Header class="mt-2 mb-3"/>
-      <div class="flex-1 h-full flex flex-row">
+      <div class="flex-1 h-full flex flex-row pt-4">
         <div class="xl:w-1/5 lg:w-1/4 sm:w-1/3 h-full mr-4">
           <NavigationMenu class="h-full" />
         </div>
