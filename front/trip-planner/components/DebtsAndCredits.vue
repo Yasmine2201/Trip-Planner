b93148f5339d53767223bd83557b11b95ce5ce92
Yasmine <yasmineazib66@gmail.com>
2025-01-17 19:34:50 +0100

    added tabs for budget

---
+++ front/trip-planner/components/DebtsAndCredits.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+const { t } = useI18n();
+
+</script>
+
+<template>
+
+   <div>
+    <p>{{ t('budget.debts-and-credits-description') }}</p>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ front/trip-planner/components/Expenses.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { t } = useI18n();
+</script>
+
+<template>
+<div>
+    <p>{{ t('budget.expenses-description') }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
