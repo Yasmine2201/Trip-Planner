0d851de8e65b433fbe4cbeb9459c8381f461b608
Yasmine <yasmineazib66@gmail.com>
2025-01-17 19:34:50 +0100

    added tabs for budget

---
+++ front/trip-planner/pages/home/trips/[tripId]/budget.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+definePageMeta({
+title: 'budget.title',
+requiresAuth: true,
+layout: 'navigation'
+});
+const { t } = useI18n();
+
+const budgetItems = [{
+  label: t('budget.debts-and-credits'),
+  icon: 'material-symbols:account-balance-wallet',
+  content: t('budget.debts-and-credits-description'),
+  component: 'DebtsAndCredits'
+}, {
+  label: t('budget.expenses'),
+  icon: 'solar:banknote-2-linear',
+  content: t('budget.expenses-description'),
+  component: 'Expenses'
+}]
+
+</script>
+
+<template>
+  <UTabs :items="budgetItems">
+    <template #item ="{item}">
+      <Expenses v-if="item.component === 'Expenses'" />
+      <DebtsAndCredits v-if="item.component === 'DebtsAndCredits'" />
+    </template>
+  </UTabs>
+
+</template>
