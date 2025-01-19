f07cac7c61e1961749f071a271b5b6f484719f4b
ThibClnt <thibaut.colnot@efrei.net>
2025-01-19 18:27:33 +0100

    Small corrections

--- back/budget/models.py
+++ back/budget/models.py
@@ -7,9 +7,9 @@ from visits.models import Visit
 
 
 def validate_category(value):
-    valid_categories = ['Food', 'Transport', 'Accommodation', 'Activity', 'Others']
-    if value not in valid_categories:
+    if value not in map(lambda x: x[0], ExpenseCategory.CATEGORY_CHOICES):
         raise ValidationError(f'{value} is not a valid category')
+
 
 # Create your models here.
 class ExpenseCategory(models.Model):
@@ -20,7 +20,8 @@ class ExpenseCategory(models.Model):
         ('Activity', 'Activity'),
         ('Others', 'Others'),
     ]
-    name = models.CharField(max_length=255, primary_key=True, choices=CATEGORY_CHOICES, default='Others', validators=[validate_category])
+    name = models.CharField(max_length=255, primary_key=True, choices=CATEGORY_CHOICES, default='Others',
+                            validators=[validate_category])
 
     def save(self, *args, **kwargs):
         self.full_clean()  # Ensures all validators, including choices, are applied
@@ -29,10 +30,11 @@ class ExpenseCategory(models.Model):
     def __str__(self):
         return f'Category {self.name}'
 
+
 class ExpenseGroup(models.Model):
     expense_group_id = models.AutoField(primary_key=True)
-    visit = models.ForeignKey(Visit,on_delete=models.CASCADE, null=True)
-    trip_participation = models.ForeignKey(TripParticipation,on_delete=models.CASCADE)
+    visit = models.ForeignKey(Visit, on_delete=models.CASCADE, null=True)
+    trip_participation = models.ForeignKey(TripParticipation, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField()
 
@@ -42,15 +44,14 @@ class ExpenseGroup(models.Model):
 
 class Expense(models.Model):
     expense_id = models.AutoField(primary_key=True)
-    trip_participation = models.ForeignKey(TripParticipation,on_delete=models.CASCADE)
-    category = models.ForeignKey(ExpenseCategory,on_delete=models.CASCADE)
+    trip_participation = models.ForeignKey(TripParticipation, on_delete=models.CASCADE)
+    category = models.ForeignKey(ExpenseCategory, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField()
     planned_amount = models.FloatField(null=True, blank=True)
     actual_amount = models.FloatField(null=True, blank=True)
     is_shared = models.BooleanField(default=False)
-    expense_group = models.ForeignKey(ExpenseGroup,on_delete=models.CASCADE, null=True)
-
+    expense_group = models.ForeignKey(ExpenseGroup, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f' Expense{self.name} - {self.planned_amount} - {self.actual_amount}'
@@ -58,8 +59,8 @@ class Expense(models.Model):
 
 class ExpenseShare(models.Model):
     expense_share_id = models.AutoField(primary_key=True)
-    expense = models.ForeignKey(Expense,on_delete=models.CASCADE)
-    with_user = models.ForeignKey(User,on_delete=models.CASCADE, null=True)
+    expense = models.ForeignKey(Expense, on_delete=models.CASCADE)
+    with_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     due_amount = models.FloatField(null=True, blank=True)
     refund_amount = models.FloatField(null=True, blank=True)
     status = models.CharField(max_length=255)
