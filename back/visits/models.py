201d5676e64e3c0fb42a08e969ece7b55016f2d2
ThibClnt <thibaut.colnot@efrei.net>
2025-01-20 08:50:27 +0100

    #104 Add models and serializers for LocationPrice

--- back/visits/models.py
+++ back/visits/models.py
@@ -4,7 +4,7 @@ from core.models import User, Image
 from trips.models import Trip
 
 
-# Create your models here.
+# Location
 class Location(models.Model):
     location_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
@@ -13,6 +13,24 @@ class Location(models.Model):
     description = models.TextField()
 
 
+class LocationPicture(models.Model):
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    picture = models.ForeignKey(Image, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('location', 'picture')
+
+
+class LocationPrice(models.Model):
+    price_id = models.AutoField(primary_key=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='prices')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price_name = models.CharField(max_length=255)
+    description = models.TextField(max_length=1000, null=True, default='')
+    created_on = models.DateTimeField(null=True, auto_now=True)
+
+
+# Visit
 class Visit(models.Model):
     visit_id = models.AutoField(primary_key=True)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
@@ -20,14 +38,6 @@ class Visit(models.Model):
     name = models.CharField(max_length=255)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
-
-
-class LocationPicture(models.Model):
-    location = models.ForeignKey(Location, on_delete=models.CASCADE)
-    picture = models.ForeignKey(Image, on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ('location', 'picture')
 
 
 class VisitParticipation(models.Model):
