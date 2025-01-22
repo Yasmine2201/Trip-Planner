2397e3fc445be7a1bd83ecaa4df54576677602d7
ethan <ethansuissa@gmail.com>
2025-01-23 00:34:11 +0100

    Ajout des composants pour créer et modifier une visite : premier jet, à tester et affiner avec le back

---
+++ front/trip-planner/pages/home/trips/[tripId]/visits/create.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import VisitForm from "~/components/VisitForm.vue";
+
+definePageMeta({
+  title: 'navigation.create_visit',
+  layout: "navigation",
+  requiresAuth: true,
+});
+
+const { t } = useI18n();
+
+const route = useRoute();
+const router = useRouter();
+const tripId = route.params.tripId;
+</script>
+
+<template>
+  <PageTitle :name="t('navigation.create_visit')">
+    <template v-slot:actions>
+      <UButton @click="router.back()" icon="i-heroicons-arrow-uturn-left" class="mr-2" :title="t('misc.back')" color="gray" />
+    </template>
+  </PageTitle>
+  <VisitForm :trip_id=tripId mode="create" />
+</template>
