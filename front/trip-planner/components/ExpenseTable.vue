eb8d983f3b023b366e1f2be45fe79d622581a402
ThibClnt <thibaut.colnot@efrei.net>
2025-01-27 02:09:47 +0100

    ExpenseTable in component

---
+++ front/trip-planner/components/ExpenseTable.vue
@@ -0,0 +1,209 @@
+<script setup lang="ts">
+import { CategoryValues } from "~/schemas/expense";
+
+interface Props {
+  expenses: Expense[],
+  tripId: number,
+  displayVisit?: boolean,
+  categoryFilter?: string,
+  selectable?: boolean,
+}
+
+const props = defineProps<Props>();
+const { tripId, categoryFilter: categoryQuery = undefined, displayVisit = true, selectable = false } = props;
+
+const { expenses: expenses } = toRefs(props);
+
+const {t} = useI18n();
+const toast = useToast();
+const route = useRoute();
+const router = useRouter();
+
+const selected = defineModel<Expense[]>() as ModelRef<Expense[]>;
+if (selected.value == undefined && selectable) {
+  selected.value = [];
+} else if (!selectable) {
+  selected.value = undefined;
+}
+
+const columns = computed(() => {
+  const col = [
+    {
+      key: 'select'
+    },
+    {
+      key: 'name',
+      label: t('expense.name'),
+      sortable: true,
+    },
+    {
+      key: 'planned_amount',
+      label: t('expense.planned-amount-short'),
+      sortable: true
+    },
+    {
+      key: 'actual_amount',
+      label: t('expense.actual-amount-short'),
+      sortable: true
+    },
+    {
+      key: 'category',
+      label: t('expense.category'),
+      sortable: true
+    },
+    {
+      key: 'visit',
+      label: t('expense.visit'),
+    },
+    {key: 'actions'}
+  ].filter(column => column.key !== 'visit' || displayVisit);
+  console.debug(col, displayVisit);
+  return col;
+});
+
+const categoryOptions = computed(() => [
+  { label: t('misc.all'), value: '' },
+  ...Object.values(CategoryValues).map(value => ({
+    label: getCategoryLabel(value),
+    value: value,
+  }))
+]);
+
+// Methods
+const getCategoryLabel = (category: string) => t(`expense.categories.${category.toLowerCase()}`);
+
+const deleteExpense = async (expenseId: number) => {
+  const {status} = await useApiFetch(`/trips/${tripId}/budget/expenses/${expenseId}`, {
+    method: 'DELETE'
+  });
+
+  if (status.value === 'success') {
+    expenses.value = expenses.value.filter((expense) => expense.expense_id !== expenseId);
+    toast.add({
+      title: t('misc.deletion'),
+      description: t('misc.success'),
+      icon: 'i-heroicons-check-badge',
+      color: "green",
+      timeout: 2000,
+    });
+  } else {
+    toast.add({
+      title: t('misc.deletion'),
+      description: t('misc.error'),
+      icon: 'i-heroicons-x-circle',
+      color: "red",
+      timeout: 2000,
+    });
+  }
+};
+
+// Category filter
+const selectedCategory = reactive({value: categoryQuery ?? ''});
+
+const filteredExpenses = computed(() => {
+  if (!selectedCategory.value) return expenses.value;
+  else {
+    return expenses.value.filter(expense => expense.category === selectedCategory.value);
+  }
+});
+
+</script>
+
+<template>
+
+  <div class="flex justify-between mb-5">
+    <div class="flex-1">
+      <slot name="caption" />
+    </div>
+
+    <UFormGroup>
+      <USelectMenu
+          v-model="selectedCategory.value"
+          :color="selectedCategory.value ? 'primary' : 'gray'"
+          :options="categoryOptions"
+          :placeholder="t('expense.placeholders.filter-by-category')"
+          class="min-w-48"
+          icon="fa6-solid:filter"
+          value-attribute="value"
+      />
+    </UFormGroup>
+  </div>
+
+  <UTable
+      v-model="selected"
+      :columns="columns"
+      :rows="filteredExpenses"
+      :empty-state="{ icon: 'i-heroicons-circle-stack-20-solid', label: t('misc.no-items') }"
+  >
+    <template #name-data="{ row }">
+      <UTooltip>
+        <span class="font-semibold underline text-left">{{ row.name }}</span>
+
+        <template #text>
+          <p class="font-bold">{{ t('expense.description') }}:</p>
+          <p>{{ row.description }}</p>
+        </template>
+      </UTooltip>
+
+    </template>
+
+    <template #planned_amount-data="{ row }">
+      <span v-if="row.planned_amount">{{
+          row.planned_amount?.toLocaleString(undefined, {minimumFractionDigits: 2})
+        }} €</span>
+      <span v-else>-</span>
+    </template>
+
+    <template #actual_amount-data="{ row }">
+      <span v-if="row.actual_amount">{{
+          row.actual_amount?.toLocaleString(undefined, {minimumFractionDigits: 2})
+        }} €</span>
+      <span v-else>-</span>
+    </template>
+
+    <template #category-data="{ row }">
+      <span>{{ getCategoryLabel(row.category) }}</span>
+    </template>
+
+    <template #visit-data="{ row }" v-if="displayVisit" >
+      <p v-if="row.visit == null"> {{ t('misc.not-defined') }} </p>
+      <p v-else>
+        <UTooltip>
+          <a :href="`/home/trips/${tripId}/visits/${row.visit?.visit_id}`" class="underline">{{ row.visit.name }}</a>
+          <template #text>
+            <span>{{ t('expense.visit-tooltip') }}</span>
+          </template>
+        </UTooltip>
+      </p>
+    </template>
+
+    <template #actions-data="{ row }">
+      <div class="flex space-x-2 items-center">
+        <div class="mr-4 pt-1">
+          <UTooltip v-if="row.is_shared" :title="t('expense.yes')">
+            <UIcon class="w-5 h-5 aspect-1" color="gray" name="ic:sharp-people-alt"/>
+          </UTooltip>
+          <UTooltip v-else :title="t('expense.no')">
+            <UIcon class="w-5 h-5 aspect-1" color="gray" name="material-symbols-light:person-rounded"/>
+          </UTooltip>
+        </div>
+        <UButton
+            :to="`/home/trips/${tripId}/budget/expenses/${row.expense_id}/edit`"
+            color="primary"
+            icon="i-heroicons-pencil-square"
+            size="xs"
+            square
+            variant="solid"
+        />
+        <UButton
+            color="red"
+            icon="i-heroicons-trash"
+            size="xs"
+            square
+            variant="solid"
+            @click="deleteExpense(row.expense_id)"
+        />
+      </div>
+    </template>
+  </UTable>
+</template>
