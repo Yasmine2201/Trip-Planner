86216d75639655a193725512d9ea818a6bc8d0e5
ThibClnt <thibaut.colnot@efrei.net>
2025-01-29 17:06:32 +0100

    #129 Move trips table to new TripTable component

--- front/trip-planner/pages/home/index.vue
+++ front/trip-planner/pages/home/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 
 import type { Trip } from '~/types';
+import type {AsyncData} from "#app";
 
 definePageMeta({
   title: 'navigation.home',
@@ -9,63 +10,9 @@ definePageMeta({
 });
 
 const { t, locale } = useI18n();
-const toast = useToast();
 
-const sort = ref({
-  column: 'start_date',
-  direction: 'desc'
-})
+const { data: rows }: AsyncData<Trip[], any> = await useApiFetch<Trip[]>('/trips');
 
-const columns = computed(() => [
-  {
-    key: 'trip_name',
-    label: t('trips.name'),
-    sortable: true
-  },
-  {
-    key: 'start_date',
-    label: t('trips.start'),
-    sortable: true,
-    direction: 'desc' as const
-  },
-  {
-    key: 'end_date',
-    label: t('trips.end'),
-    sortable: true,
-    direction: 'desc' as const
-  },
-  {
-    key: 'actions',
-  }
-]);
-
-const { data: rows, status } = await useApiFetch<Trip[]>('/trips');
-const computedRows = computed(() => rows.value ?? []);
-
-const deleteTrip = async (tripId: number) => {
-  const { status } = await useApiFetch(`/trips/${tripId}`, {
-    method: 'DELETE'
-  });
-  if (status.value === 'success') {
-    rows.value = rows.value.filter((trip) => trip.trip_id !== tripId);
-    toast.add({
-      title: t('misc.deletion'),
-      description: t('misc.success'),
-      icon: 'i-heroicons-check-badge',
-      color: "green",
-      timeout: 2000,
-    });
-  }
-  else {
-    toast.add({
-      title: t('misc.deletion'),
-      description: t('misc.error'),
-      icon: 'i-heroicons-x-circle',
-      color: "red",
-      timeout: 2000,
-    });
-  }
-}
 </script>
 
 <template>
@@ -81,56 +28,7 @@ const deleteTrip = async (tripId: number) => {
       />
     </template>
   </PageTitle>
-  <UTable
-      :columns
-      :sort
-      :rows="computedRows"
-      :loading="status === 'pending'"
-      :loading-state="{ icon: 'i-heroicons-arrow-path-20-solid', label: t('misc.loading') }"
-      :progress="{ color: 'primary', animation: 'carousel' }"
-      :empty-state="{ icon: 'i-heroicons-circle-stack-20-solid', label: t('misc.no-items') }"
-  >
-    <template #trip_name-data="{ row }">
-      <NuxtLink :to="`/home/trips/${row.trip_id}`">{{ row.trip_name }}</NuxtLink>
-    </template>
-
-    <template #start_date-data="{ row }">
-      {{ new Date(row.start_date).toLocaleDateString([locale], { dateStyle: 'full' }) }}
-    </template>
-
-    <template #end_date-data="{ row }">
-      {{ new Date(row.end_date).toLocaleDateString([locale], { dateStyle: 'full' }) }}
-    </template>
-
-    <template #actions-data="{ row }">
-      <div class="flex space-x-2">
-        <UButton
-            icon="i-heroicons-eye"
-            size="xs"
-            color="gray"
-            square
-            variant="solid"
-            :to="`/home/trips/${row.trip_id}`"
-        />
-        <UButton
-            icon="i-heroicons-pencil-square"
-            size="xs"
-            color="primary"
-            square
-            variant="solid"
-            :to="`/home/trips/${row.trip_id}/edit`"
-        />
-        <UButton
-            icon="i-heroicons-trash"
-            size="xs"
-            color="red"
-            square
-            variant="solid"
-            @click="deleteTrip(row.trip_id)"
-        />
-      </div>
-    </template>
-  </UTable>
+  <TripTable v-model:trips="rows" />
 </template>
 
 <style scoped>
