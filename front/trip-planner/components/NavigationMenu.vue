383bcc9aa0dda7b1539a7b6514e8267dc891f318
ThibClnt <thibaut.colnot@efrei.net>
2025-01-31 02:28:20 +0100

    #161 Remove trip invitation duplicate

--- front/trip-planner/components/NavigationMenu.vue
+++ front/trip-planner/components/NavigationMenu.vue
@@ -28,11 +28,6 @@ const links = computed(() => {
     ],
     [
       {
-        label: t('navigation.trips_invitation'),
-        icon: "i-uil-plane",
-        to: "/home/trips/invitations/",
-      },
-      {
         label: t('navigation.my-trips'),
         icon: "material-symbols:trip-outline",
         to: "/home",
